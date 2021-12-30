c15f198d3fc92bfbdb7db2b66714857e4cb386f3
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2021-12-30 11:44:20 +0900

    Add files via upload

--- VariableTest.java
+++ VariableTest.java
@@ -1,58 +1,29 @@
-package ch2;
+package ch6;
+
+class A {
+	
+	int i; //인스턴스 변수=객체 변수=필드 변수=멤버 변수 - 인스턴스변수는 자동초기저장값이 정해져 있다.
+	void m() {
+		int j = 0; //m메소드가 가지고 있는 지역 변수 -지역 변수는 자동초기화값이 정해져 있지 않다. 반면 인스턴스변수는 자동초기저장값이 저장되어 있다. 그러므로 지역변수는 값을 꼭 초기화 해줘야 한다.
+		System.out.println(i+j);
+		for(int k=0 ; k<10 ; k++) { //for블록 지역 변수
+			System.out.println(k);
+		}
+		//System.out.println(k); for블록 밖에 있기 때문에 정 쓰고 싶다면 for문 바로 위에 쓰기 int k=0; 이렇게..
+	}
+	void m2() {
+		//System.out.println(i-j);
+		System.out.println(i); //자바에서 실행하려면 main 메서드가 있어야 한다. 하지만 A클래스에는 메인 메서드가 없다. 그러므로 밑에 메인 메서드에서 A클래스를 불러와야 실해행된다.
+	}
+}
 
 public class VariableTest {
 
 	public static void main(String[] args) {
-		//국어 영어 수학 점수 선언 + 값 할당(or초기화)
-		int kor = 100;
-		int eng = 50;
-		int mat = 75;
-		//오류 int sci = 99.9;
 		
-		//총점 : 선언
-		int sum;
-		
-		//평균 : 선언
-		int avgInt;
-		double avgDouble ;
-		
-		System.out.println("국어 점수는="+kor); //String + 정수실수 , String + String ==> 요소 결합 출력
-		System.out.println(kor+eng+mat); //정수, 실수 + 정수, 실수 ==> 산술연산 덧셈
-		System.out.println(eng);
-		System.out.println(mat);
-		
-		//영어 너무 낮아서 영어 조정 2배
-		eng = 50 * 2;
-		
-		//sum값 할당
-		sum = kor + eng + mat;
-		avgInt = sum / 3;
-		avgDouble = sum / 3.0;
-		
-		
-		System.out.println(sum);
-		System.out.println(avgInt);
-		System.out.println(avgDouble);
-		
-		//데이터타입 변수명 = 값
-		boolean flag = true;
-		System.out.println(flag);
-		flag = false;
-		System.out.println(flag);
-		//오류 boolean flag2 = True;
-		
-		char c1 = '이'; //문자 컴퓨터 이진수 - unicode - 'a' 대응 숫자 변환 = 97
-		char c2 = '학';
-		char c3 = '생';
-		char c4 = '\n';
-		
-		System.out.print(c1 + "\n");
-		System.out.print(c2);
-		System.out.println(c3);
-		//System.out.println((char)c1 + "" + (char)c2 + (char)c3);
-		
-		//기본 제공 데이터타입 여러 문자 묶어서 선언 불가능
-		//사용자 데이터타입 정의 사용 가능 = (1. String 클래스 jdk 포함 => 2. 사용자)
-		String name = "이학생";
+		A a1 = new A();
+		a1.m2();
+		a1.m();
+
 	}
 }
