4341abb984c7d5af146367292965ef736a02c549
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2021-12-31 08:57:32 +0900

    Add files via upload

---
+++ Join.java
@@ -0,0 +1,23 @@
+package test;
+
+class Member {
+	
+	int id; //아이디
+	int pw; //비밀번호
+	String name; //이름
+	int age; //나이
+	
+}
+
+public class Join {
+
+	public static void main(String[] args) {
+
+		public void insert {
+			
+		}
+		
+		
+	}
+
+}
