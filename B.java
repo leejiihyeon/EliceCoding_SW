7ca72026e8f1b5d987535109dc9e19d3fc4e30ea
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2021-12-31 16:18:33 +0900

    Add files via upload

---
+++ B.java
@@ -0,0 +1,17 @@
+package test; //현재 클래스 저장 위치 설정
+
+//다른 클래스의 패키지명 알려줌
+import java.util.ArrayList;
+import java.util.Date;
+
+public class B {
+
+	public static void main(String[] args) {
+
+		multi.A a1 = new multi.A();
+		Date now = new Date(); //현재시각
+		System.out.println("현재시각 : "+now);
+		ArrayList list = new ArrayList();
+
+	}
+}
