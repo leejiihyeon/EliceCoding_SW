1d5b6dc3458a2fd4d3cb304d2efa4b452d6e6e52
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2022-01-05 11:35:23 +0900

    Add files via upload

---
+++ InterfaceTest.java
@@ -0,0 +1,35 @@
+class Teacher {
+	void teach() {
+		System.out.println("가르치다");
+	}
+}
+
+class StudentWorker extends Teacher implements Worker, Student {
+
+	@Override
+	public void study() {
+		System.out.println("공부 하다");
+	}
+
+	@Override
+	public void work() {
+		System.out.println("학교 업무를 보다");
+	}
+
+	@Override
+	public void lunch() {
+		System.out.println("점심 먹다");
+	}
+//
+}
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+	StudentWorker sw = new StudentWorker();
+	sw.study(); //인터페이스 구현
+	sw.lunch(); //인터페이스 구현(2개 인터페이스 포함)
+	sw.work(); //인터페이스 구현
+	sw.teach(); //클래스 상속
+	
+	}
+}
