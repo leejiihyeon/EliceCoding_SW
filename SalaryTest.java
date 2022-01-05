f596c26bd456a6cd3f76e3cce2ec3d42e435245d
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2022-01-05 12:21:33 +0900

    Add files via upload

--- SalaryTest.java
+++ SalaryTest.java
@@ -18,6 +18,7 @@ class Employee {
 	Employee() {}
 	void calcTotalSalary() {
 		totalSalary = baseSalary + generalFee;
+		//100문장 정의
 	}
 } //Employee end
 
@@ -46,18 +47,18 @@ class Engineer extends Employee {
 		super(id, name, baseSalary, generalFee);
 		this.certiFee = certiFee;
 		this.techFee = techFee;
-		
 	}
 	@Override
 	void calcTotalSalary() {
 		//totalSalary= baseSalary + generalFee + managerFee;
 		super.calcTotalSalary();
 		totalSalary = totalSalary + certiFee + techFee;
-}
+	}
+} //Engineer end
 
-public class SalaryTest extends Employee {
+class Secretary extends Employee {
 	int secFee;
-	public SecretKey(int id, String name, int baseSalary, int generalFee, int certiFee, int techFee) {
+	public Secretary(int id, String name, int baseSalary, int generalFee, int secFee ) {
 		super(id, name, baseSalary, generalFee);
 		this.secFee = secFee;
 	}
@@ -67,19 +68,22 @@ public class SalaryTest extends Employee {
 		super.calcTotalSalary();
 		totalSalary = totalSalary + secFee;
 	}
-}
+} 
 	class SalaryTest {
 	public static void main(String[] args) {
 		
 		Employee e [] = new Employee[4];
+		// 1. 상속 2. 생성자(타입 순서 갯수...)
 		e[0] = new Employee (1000, "이사원", 10000, 5000);
-		e[1] = new Manager (1000, "이사원", 10000, 5000);
-		e[2] = new Engineer (1000, "이사원", 10000, 5000);
-		e[3] = new Secretary (1000, "이사원", 10000, 5000);
-		
+		e[1] = new Manager (2000, "김간부", 20000, 10000, 10000);
+		e[2] = new Engineer (3000, "박기술", 15000, 7500, 5000, 5000);
+		e[3] = new Secretary (4000, "최비서", 15000, 7000, 10000);
+		//배열 내의 각 객체의 급여계산 메소드 호출하고
+		//사번, 이름, 본봉, 총급여 출력
+		System.out.println("사번, 이름, 본봉, 총급여");
 		for(int i=0 ; i<e.length ; i++) {
 			e[i].calcTotalSalary(); //e[i].오버라이딩모세드 --> 각 객체 메소드 실행
-			System.out.println(e[i].id + ":" + e[i].name + ":" + e[i].baseSalary + ":" + e[i].totalSalary)
+			System.out.println(e[i].id +":" + e[i].name +":" +e[i].baseSalary +":" + e[i].totalSalary);
 		} //for end
-
+	}
 }
