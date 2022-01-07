f8b7bc4341d5d75e322d185f100e02adb745d2f9
LeeJiHyeon <92428769+leejiihyeon@users.noreply.github.com>
2022-01-07 10:30:30 +0900

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,8 +1,17 @@
+package test;
+/*
+교육생 클래스에는 
+<변수>
+이름, 소속(dept), 과정명(course ), 교육비, 부가금(addmoney), 환급금(refund)
+나타내는 변수를 정의합니다.
+*/
+public class Student {
 
-public interface Student {
-	
-	/*public static final*/String name = "학생";
-	/*abstract public*/void study();
-	/*abstract public*/void lunch();
-	
+		String name;
+		String dept;
+		String course;
+		double pay;
+		double addmoney;
+		double refund;
+		
 }
