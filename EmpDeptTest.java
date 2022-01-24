ea63f7d0e4941758b9812fc3540caa4b4213b886
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-01-24 12:13:38 +0900

    Add files via upload

--- EmpDeptTest.java
+++ EmpDeptTest.java
@@ -1,12 +1,8 @@
-package jdbc;
-/*  employees 테이블에서 6월 입사자의 사번, 이름, 급여, 입사일을 조회하여
- * Employee 객체로 생성한 후에 ArrayList로 저장하고 출력하는 자바 프로그램을 구현하시오. */
 
 
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,54 +12,45 @@ public class EmpDeptTest {
 
 	public static void main(String[] args) {
 		EmpDeptTest t = new EmpDeptTest();
-		try {
-			
-			String []mon = {"02", "06","12"};
-			ArrayList<Employee> list = t.selectEmp(mon);
-			for(Employee e : list) {
-				System.out.println(e.toString());
-			}
-		}catch(Exception e) {
-			e.printStackTrace(); //예외이름+상황설명메시지출력
+		ArrayList<Employee> list = t.selectEmp("12");
+		for(Employee e : list) {
+			System.out.println(e);
 		}
 	}//main
 	
-	ArrayList<Employee> selectEmp(String[] month) throws ClassNotFoundException , SQLException{
-		ArrayList<Employee> list =  new ArrayList();
-		
+	ArrayList<Employee> selectEmp(String month){
+		ArrayList<Employee> list = new ArrayList<Employee>();
+		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = 
-		DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employeesdb", "emp", "emp");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employeesdb", "emp", "emp");
+		String selectSQL = "select employee_id, first_name, salary, date_format(hire_date, '%Y년도%m월%d일') as hire_date from  employees "
+				+ " where hire_date like '_____" + month + "%'";
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(selectSQL);
 		
-		/*Statement st = conn.createStatement();
-		String sql ="select employee_id, first_name, salary, date_format(hire_date, '%Y년도 %m월 %d일') as hire from employees"
-				+ " where substr(hire_date, 6, 2)='"  + month  + "'";
-		ResultSet rs = st.executeQuery(sql);
-		*/
-		
-		String sql = "select employee_id, first_name, salary, date_format(hire_date, '%Y년도 %m월 %d일') as hire from employees"
-				+ " where substr(hire_date, 6, 2)=?";  // "...hire_date='" + 변수 + "'";
-		PreparedStatement pt = conn.prepareStatement(sql);
-		ResultSet rs = null;
-		
-		for(int i = 0; i < month.length; i++) {
-			pt.setString(1, month[i]); //자바(sTRING) --> MYSQL(CHAR,VARCHAR- '' 붙인다)
-			rs = pt.executeQuery();
-			while(rs.next()) {
-				int id = rs.getInt("employee_id");
-				String name = rs.getString("FIRST_NAME");
-				double salary = rs.getDouble("salary");
-				String hire = rs.getString("hire"); // 날짜--> 문자열 내부 취급
-				Employee e = new Employee(id, name, salary, hire.toString());
-				list.add(e);
-			}
+		while(rs.next()) {
+			int id = rs.getInt("employee_id");
+			String name = rs.getString("first_name");
+			double salary = rs.getDouble("salary"); 
+			String hire_date = rs.getString("hire_date");
+			//System.out.println(id + "\t" + name + "\t" + salary + "\t" + hire_date);
+			Employee e = new Employee(id, name, salary, hire_date);
+			list.add(e);
 			
 		}
 		rs.close();
-		pt.close();
+		st.close();
 		conn.close();
-	
-		return list;//연결해제 이후 다른 메소드 결과 조회 가능
+		System.out.println("mysql db 연결 해제 성공");		
+		}
+		catch(ClassNotFoundException e) {
+			System.out.println("mysql driver 미설치 또는 드라이버이름 오류");
+		}
+		catch(SQLException e) {
+			System.out.println("db접속오류이거나 sql문장오류");
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 }//EmpDeptTest end
@@ -73,6 +60,7 @@ class Employee{
 	String first_name;
 	double salary;
 	String hire_date;
+	
 	public Employee(int employee_id, String first_name, double salary, String hire_date) {
 		super();
 		this.employee_id = employee_id;
@@ -80,17 +68,11 @@ class Employee{
 		this.salary = salary;
 		this.hire_date = hire_date;
 	}
+
 	@Override
 	public String toString() {
-		return employee_id + "\t" + first_name + "\t" + salary + "\t" + hire_date;
-	}
+		return "Employee [employee_id=" + employee_id + ", first_name=" + first_name + ", salary=" + salary
+				+ ", hire_date=" + hire_date + "]";
+	} 
 	
 }
-
-
-
-
-
-
-
-
