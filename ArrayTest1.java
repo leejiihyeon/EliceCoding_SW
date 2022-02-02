24588c4f9e968ccdab8280198c1fa46e2e6f3cac
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-02-02 13:43:41 +0900

    Add files via upload

--- ArrayTest1.java
+++ ArrayTest1.java
@@ -3,55 +3,54 @@ package ch5;
 public class ArrayTest1 {
 
 	public static void main(String[] args) {
-		
+
 		String names[] = {"이학생", "최신입", "김대리", "박과장", "최부장"};
-//		System.out.println("학생수 : "+names.length);
-//		for(int i=0 ; i<names.length ; i++) {
-//			System.out.println(names[i]);
-//		}
+		//System.out.println("학생수="+names.length);
+		//for(int i = 0; i < names.length; i++) {
+		//	System.out.println(names[i]);
+		//}
 		
+		int scores[] = new int[names.length];
 		
-		//46개 점수 저장
-		int scores[] = new int[10];
-		
-		//1~100 점수 배열 랜덤 저장
-		for(int i=0 ; i<scores.length ; i++) {
+// 1-100 점수 배열 랜덤 저장
+		for(int i = 0; i < scores.length; i++) {
 			scores[i] = (int)(Math.random() * 100) + 1;
-			System.out.println(i+" : "+scores[i]);
+			System.out.print(names[i] + ":" + scores[i] + " , ");
 		}
 		System.out.println();
-		System.out.println("점수의 총 갯수 : "+scores.length);
-		
-		//총점, 평균
+		System.out.println("점수 총갯수=" + scores.length);
+		//총점,평균
 		int sum = 0;
 		int avg;
-		for(int i=0 ; i<scores.length ; i++) {
+		for(int i = 0; i < scores.length; i++) {
 			sum = sum + scores[i];
 		}
-		avg = sum / 46;
-		System.out.println("총점 : "+sum);
-		System.out.println("평균 : "+avg);
+		avg = sum / scores.length;
+		System.out.println("총점=" + sum);
+		System.out.println("평균" + avg);
 		
-		//1등 점수
-		int max=0; //max=0, scores[i]=xxxx
+		//1등점수
+		int max = 0; //max=100 , scores[i]=xxxx
 		String maxName = "";
-		for(int i=0 ; i<scores.length ; i++) {
-		if(scores[i]>max) {
-			max=scores[i];
-			maxName=names[i];
-			//System.out.println(max);
-		} //if end
-		} //for end
-		System.out.println("1등 점수 : "+maxName+" : "+max);
+		for(int i = 0; i < scores.length; i++) {
+		if(scores[i] > max) {
+			max = scores[i];
+			maxName = names[i];
+		}//if end
+		}//for end
 		
-		//꼴등 점수
-		int min=101; //min=101, scores[i] = 1
-		for(int i=0 ; i<scores.length ; i++) {
-		if(scores[i]<min) {
-			min=scores[i];
-		}
-		}
-		System.out.println("꼴등 점수 : "+min);
-	
+		System.out.println("1등점수="+maxName + ":" +  max); //1등점수 xxxx : xxx
+		
+		//꼴등점수
+		int min = 101; //min=999  scores[i] = 1
+		for(int i = 0; i < scores.length; i++) {
+		if(scores[i] < min) {
+			min = scores[i];
+			System.out.println(min);
+		}//if end
+		}//for end
+		
+		System.out.println("꼴등점수="+min);		
 	}
+
 }
--- CellPhoneMain.java
+++ CellPhoneMain.java
@@ -1,79 +1,91 @@
 package test;
 
-class CellPhone {
-	String model; //핸드폰 모델 번호
-	double battery; //남은 배터리 양
-	
-	CellPhone(String model) { //모델 번호를 인자로 받는 생성자
-		this.model = model; 
+class CellPhone	{
+	String model;	// 핸드폰 모델 번호
+	double battery;	// 남은 배터리 양
+
+	CellPhone(String model){	// 모델 번호를 인자로 받는 생성자
+		this.model = model;
 	}
-	
-	void call(int time) throws IllegalArgumentException {
-		
-		if(time<0) {
-			throw new IllegalArgumentException("통화시간입력오류"); //중단
+
+	void  call(int time) throws IllegalArgumentException {	
+		if(time < 0) {
+			throw new IllegalArgumentException("통화시간입력오류");//중단
 		}
-		System.out.println("통화시간 : "+ time +"분");
+		System.out.println("통화시간 : " + time + "분");
 		battery = battery - time*0.5;
-		if(battery<0) {
-			battery=0;
+		if(battery < 0) {
+			battery = 0;
 		}
-	} //CALL END
+	}//CALL END
 	
-	void charge(int time) throws IllegalArgumentException {
+	void  charge(int time) throws IllegalArgumentException {	
 		if(time > 100) {
 			throw new IllegalArgumentException("충전시간입력오류");
 		}
+		System.out.println("충전시간 : " + time + "분");
 		battery = battery + time * 3;
 		if(battery > 100) {
 			battery = 100;
 		}
-	} //charge end
+	}//charge end
 	
-	void printBattery() { //남은 배터리양을 출력한다
+	void  printBattery() {	// 남은 배터리 양을 출력한다.
 		System.out.println("남은 배터리 양 : " + battery);
 	}
-	
-	boolean isSame(CellPhone other) {
+
+	boolean  isSame(CellPhone other) {
 		//model명 같으면 같은 기종
 		//대소문자 구분없이
 		return model.equalsIgnoreCase(other.model);
-	} //isSame 끝
-} //CellPhone 끝
+	}//isSame
+}//CellPhone end
 
+/*<<CellPhoneMain 실행결과>>
+충전 시간 : 20분
+남은 배터리 양 : 60.0
+통화 시간 : 300분
+남은 배터리 양 : 0.0
+충전 시간 : 50분
+남은 배터리 양 : 100.0
+통화 시간 : 40분
+남은 배터리 양 : 80.0
+통화시간입력오류
+동일 모델입니다. 
+*/
 public class CellPhoneMain {
+public static void main(String[] args) { 
+	CellPhone myPhone =  new CellPhone("GALAXY-9");
+		
+	try {
 
-	public static void main(String[] args) {
+		myPhone.charge( 20 );  //20분간 충전을 한다.
+		myPhone.printBattery();
 		
-		CellPhone myPhone = new CellPhone("GALAXY-9");
+		myPhone.call( 300 );  //300분간 통화를 한다.
+		myPhone.printBattery();
 		
-		try {
-			
-			myPhone.charge(20); //20분간 충전을 한다
-			myPhone.printBattery();
-
-			myPhone.call(300); //300분간 통화를 한다.
-			myPhone.printBattery();
-			
-			myPhone.charge( 50 );  //50분간 충전을 한다.
-			myPhone.printBattery();
-			
-			myPhone.call( 40 );  //40분간 통화를 한다.
-			myPhone.printBattery();
-			
-			myPhone.call( -20 );  //통화시간입력오류
-		}
-		catch(IllegalArgumentException e) {
-			System.out.println(e.getMessage());	
-		}
+		myPhone.charge( 50 );  //50분간 충전을 한다.
+		myPhone.printBattery();
 		
-			CellPhone yourPhone = new CellPhone("galaxy-9");
-			
-			if( myPhone.isSame(yourPhone) ) {
-				System.out.println("동일 모델입니다.");
-			} else {
-				System.out.println("다른 모델입니다.");
-			} 
-		} 
+		myPhone.call( 40 );  //40분간 통화를 한다.
+		myPhone.printBattery();
+		
+		myPhone.call( -20 );  //통화시간입력오류
 	}
+	catch(IllegalArgumentException e) {
+		System.out.println(e.getMessage());	
+	}
+	
+		CellPhone yourPhone = new CellPhone("galaxy-9");
+		
+		if( myPhone.isSame(yourPhone) ) {
+			System.out.println("동일 모델입니다.");
+		} else {
+			System.out.println("다른 모델입니다.");
+		} 
+	} 
+}
+
+
 
