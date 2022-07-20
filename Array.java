6b6a5fa0214a9c57112939a0d37d0b339ff43657
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-07-20 11:14:59 +0900

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,34 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+//ArrayList를 사용한 집합 구현 실습 (집합 관련 연산 사용X)
+public class Array {
+	
+	public static void main(String[] args) {
+		
+		//ArrayList
+		ArrayList<Integer> list;
+		
+		MySet a = new MySet();
+		
+		//생성자1
+		MySet() {
+			this.list = new ArrayList<Integer>();
+		}
+		
+		//생성자2
+		MySet(int[] arr) {
+			this.list = new ArrayList<Integer>();
+			
+			for(int item; arr) {
+				this.list.add(item);
+			}
+		}
+		this.list.add(x);
+	}
+	//교집합
+	public MySet retainAll 
+			
+
+}
