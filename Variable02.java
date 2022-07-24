fad180da9df31e44683b8b7e1267f78bae2b6c95
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-07-24 19:05:26 +0900

    Add files via upload

---
+++ Variable02.java
@@ -0,0 +1,61 @@
+package JavaBasic;
+
+public class Variable02 {
+
+	public static void main(String[] args) {
+
+		//Java 프로그래밍 - 변수와 자료형2
+		//1. 자료형 - 숫자
+		System.out.println("== 숫자 ==");
+
+		//1-1. 정수
+		int intNum = 10;
+		System.out.println("intNum = " + intNum); //10
+		
+		System.out.println(Integer.MIN_VALUE); //-2147483648
+		System.out.println(Integer.MAX_VALUE); //2147483647
+		
+		int intNum2 = Integer.MAX_VALUE;
+		System.out.println("intNum2 = " + intNum2); //intNum2 = 2147483647
+		int intNum3 = Integer.MAX_VALUE + 1;
+		System.out.println("intNum3 = " + intNum3); //intNum3 = -2147483648
+		long longNum = (long)Integer.MAX_VALUE + 1;
+		System.out.println("longNum = " + longNum); //longNum = 2147483648
+		
+		//1-2. 실수
+		float floatNum = 1.23f;
+		double doubleNum = 1.23;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		//1-3. 2진수 / 8진수 / 16진수
+		int numBase2 = 0b1100;
+		System.out.println("numBase2 = " + numBase2);
+		int numBase8 = 014;
+		System.out.println("numBase8 = " + numBase8);
+		int numBase16 = 0xc;
+		System.out.println("numBase16 = " + numBase16);
+		
+		
+		System.out.println("0b" + Integer.toBinaryString(numBase2));
+		System.out.println("0" + Integer.toBinaryString(numBase8));
+		System.out.println("0x" + Integer.toBinaryString(numBase16));
+		
+		//2. 자료형 - 부울
+		System.out.println("== 부울 ==");
+		boolean isPass = true;
+		System.out.println("isPass = " + isPass);
+		boolean isOk = false;
+		System.out.println("isOk = " + isOk);
+		
+		//3. 자료형 - 문자
+		System.out.println("== 문자 ==");
+		char keyFirst = 'a';
+		System.out.println("keyFirst = " + keyFirst);
+		char keyLast = 'z';
+		System.out.println("keyLast = " + keyLast);
+		System.out.println((int)keyFirst);
+		System.out.println((int)keyLast);
+		
+	}
+}
