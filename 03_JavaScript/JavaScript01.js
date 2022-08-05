80e52ef42325e16b45c68841451571da009ae7d7
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-05 10:03:06 +0900

    Add files via upload

---
+++ 03_JavaScript/JavaScript01.js
@@ -0,0 +1,13 @@
+/* 숫자 변수를 선언하고, 데이터 400을 삽입하세요. */
+var num = 400;
+
+/* 문자열 변수를 선언하고, 데이터 “bus” 를 삽입하세요. */
+var str1 = "bus";
+
+/* 문자열 변수를 선언하고, 데이터 “like” 를 삽입하세요. */
+var str2 = "like";
+
+/* 세 변수를 웹 화면에 출력하세요. */
+document.write(num);
+document.write(str1);
+document.write(str2);
