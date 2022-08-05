09460926967c35432927db514480f4133421ee68
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-05 12:03:02 +0900

    Add files via upload

---
+++ 03_JavaScript/JavaScript03.js
@@ -0,0 +1,16 @@
+/* var를 사용하여 문자열 변수 str1을 선언하고, 데이터 “Hello World” 을 삽입합니다. */
+var str1; //변수 선언
+str1  = "Hello World"; //변수 초기화
+
+/* var를 사용하여 문자열 변수 str2을 선언하고, 데이터 ‘Nice to meet you’ 을 삽입합니다. */
+var str2; //변수 선언
+str2 = 'Nice to meet you'; //변수 초기화
+
+/* var를 사용하여 문자열 변수 str3을 선언하고, 데이터 “She’s a girl” 을 삽입합니다. */
+var str3; //변수 선언
+str3 = "She's a girl";
+
+/* document.write(); 또는 document.writeln();를 사용하여 세 변수들을 출력합니다. */
+document.write(str1);
+document.write(str2);
+document.write(str3);
