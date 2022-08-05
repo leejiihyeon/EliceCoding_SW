47e6b9277087383f55bbfb38321202d9cbe422bd
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-05 10:34:33 +0900

    Add files via upload

---
+++ 03_JavaScript/JavaScript02.js
@@ -0,0 +1,17 @@
+/* var를 사용하여 변수 fruit을 선언하고, 데이터 apple을 삽입합니다. */
+var fruit; //변수 선언
+fruit = "apple"; //변수 초기화
+
+/* var를 사용하여 변수 box를 선언하고, 데이터 banana를 삽입합니다. */
+var box; //변수 선언
+box = "banana"; //변수 초기화
+
+/* document.write();을 사용하여 변수 fruit과 box의 데이터를 확인합니다. */
+document.write(fruit);
+document.write(box);
+
+/* 변수 box의 데이터를 tomato로 변경합니다. */
+box = "tomato";
+
+/* document.write();을 사용하여 변수 box의 변경된 데이터를 확인합니다. */
+document.write(box);
