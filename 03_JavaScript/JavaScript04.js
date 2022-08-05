463431bf20b047a2f55a8e9a8e722246963dbe36
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-05 12:03:20 +0900

    Add files via upload

---
+++ 03_JavaScript/JavaScript04.js
@@ -0,0 +1,10 @@
+/* var를 사용하여 숫자 변수 num1을 생성하고, 데이터 10을 삽입합니다. */
+var num1; //변수 선언
+num1 = 10; //변수 초기화
+
+/* var를 사용하여 숫자 변수 num2를 생성하고, 데이터 3.14을 삽입합니다. */
+var num2; //변수 선언
+num2 = 3.14; //변수 초기화
+
+/* document.write(num1 + num2) 를 입력해 num1과 num2를 더한 값을 알아봅니다. */
+document.write(num1+num2);
