9b9eb58d4b4c4a00cd10f51c5cfb060a3a2fc4fd
LEEJIHYEON <92428769+leejiihyeon@users.noreply.github.com>
2022-08-29 13:33:10 +0900

    Add files via upload

---
+++ 04_Node.js&Express.js/Nodejs01.js
@@ -0,0 +1,13 @@
+// 숫자와 문자열이 들어 있는 변수입니다. 수정하지 마세요!
+var num = 1030;
+var string = "hello, ";
+
+/* num 변수에 204를 더하고 그 수를 num 변수에 저장해봅시다. */
+num = num + 204;
+
+/* string 변수에 'elice!'를 더하고 그 문자열을 string 변수에 저장해봅시다. */
+string = string + 'elice!';
+
+/*  num 변수와 string 변수를 차례대로 한 줄씩 출력해봅시다. */
+console.log(num);
+console.log(string);
